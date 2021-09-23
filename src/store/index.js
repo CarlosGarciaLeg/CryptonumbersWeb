316ee7e4625cf9345f971ce5d7af1cd086c91679
javier <javiervelaz@hotmail.com>
2021-09-23 14:54:15 -0300

    metamask

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import getWeb3 from '../util/getWeb3'
 
 Vue.use(Vuex)
 
@@ -8,8 +9,29 @@ export default new Vuex.Store({
   state: {
   },
   mutations: {
+    registerWeb3Instance (state, payload) {
+      console.log('registerWeb3instance Mutation being executed', payload)
+      let result = payload
+      let web3Copy = state.web3
+      web3Copy.coinbase = result.coinbase
+      web3Copy.networkId = result.networkId
+      web3Copy.balance = parseInt(result.balance, 10)
+      web3Copy.isInjected = result.injectedWeb3
+      web3Copy.web3Instance = result.web3
+      state.web3 = web3Copy
+      }
   },
   actions: {
+    registerWeb3 ({commit}) {
+      console.log('registerWeb3 Action being executed')
+      
+      getWeb3.then(result => {
+        console.log('committing result to registerWeb3Instance mutation')
+        commit('registerWeb3Instance', result)
+      }).catch(e => {
+        console.log('error in action registerWeb3', e)
+      })
+    }
   },
   modules: {
   }
